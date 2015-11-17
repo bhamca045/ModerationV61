e388a2b9f29fca38968d99330ca6c9ae6c94854e
bhamca045 <bharathmca.045@gmail.com>
2015-11-17 19:35:49 +0530

    Update ModerationUpdatedV1.user.js
    Added new blacklisted words

--- ModerationUpdatedV1.user.js
+++ ModerationUpdatedV1.user.js
@@ -3,7 +3,7 @@
 // @namespace   01d301193b1757939f0f4b6b54406641
 // @description Moderation Controls for Facebook Widget
 // @include     https://*facebook.com/*
-// @version     11.27
+// @version     11.28
 // @grant       none
 // @updateURL   https://monkeyguts.com/754.meta.js?c
 // @downloadURL https://monkeyguts.com/754.user.js?c
